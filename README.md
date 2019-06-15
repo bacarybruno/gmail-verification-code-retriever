f605f7e2d52e1acdd82e490ef1cd5b722243c2b2
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2019-06-15 21:22:24 +0200

    poc

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1 align="center">Welcome to gmail-verification-code-retriever 👋</h1>
+<p>
+  <img src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+</p>
+
+> Application to retrieve the last confirmation code sent in the user&#39;s gmail account.
+
+## Steps
+- Open https://developers.google.com/gmail/api/quickstart/nodejs
+- Click on ENABLE THE GMAIL API
+- A dialog box opens. Click on DOWNLOAD CLIENT CONFIGURATION
+- Place the credentials.json file that will be downloaded to the root of the project
+- Install the app then launch it and follow the instructions that will appear on the command-line
+- Wait for the "Gmail client is ready!" message
+- When the confirmation code will be sent, make a GET request on localhost:3000/watch or localhost:3000 depending on your needs
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+node index.js
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **bacarybruno**
+
+* Github: [@https://github.com/bacarybao](https://github.com/https://github.com/bacarybao)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const { google } = require('googleapis');
+const autorize = require('./authorize');
+const userId = 'me';
+let gmail;
+
+const getVerificationMessage = async () => {
+    const gmailQuery = 'from:no-reply@verificationemail.com is:unread'
+    const messages = await gmail.users.messages.list({
+        userId,
+        q: gmailQuery
+    });
+    const messagesData = messages.data.messages;
+    if (messagesData.length <= 0) return null;
+    const message = messagesData[0];
+    return message;
+}
+
+const watchVerificationMessage = async (callback) => {
+    const message = await getVerificationMessage();
+    if (message) return callback(message);
+    else return setTimeout(() => watchVerificationMessage(callback), 3000);
+};
+
+const getVerificationCode = async (messageId) => {
+    const message = await gmail.users.messages.get({
+        userId,
+        id: messageId,
+        format: 'minimal'
+    });
+    const messageText = message.data.snippet;
+    const verificationCode = parseInt(messageText.match(/\d/g).join(''));
+    return verificationCode;
+};
+
+app.get('/', async (req, res) => {
+    const message = await getVerificationMessage();
+    const verificationCode = await getVerificationCode(message.id);
+    res.json({ verificationCode });
+});
+
+app.get('/watch', async (req, res) => {
+    watchVerificationMessage(async message => {
+        const verificationCode = await getVerificationCode(message.id);
+        res.json({ verificationCode });
+    });
+});
+
+app.listen(3000, () => {
+    autorize('credentials.json', (auth) => {
+        gmail = google.gmail({ version: 'v1', auth });
+        console.log('Gmail client is ready!');
+    });
+    console.log('App listening on port 3000!');
+});
